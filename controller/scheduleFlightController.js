a54e4b136fafafcfa111f74bd7d6006170a2ec5a
NiratKashom <earthenly@gmail.com>
2021-10-12 11:18:36 +0700

    <controller>can update flight to db and res to FE

--- controller/scheduleFlightController.js
+++ controller/scheduleFlightController.js
@@ -41,25 +41,33 @@ exports.createFlight = async (req, res, next) => {
   }
 };
 
-// exports.editList = async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const { title, status } = req.body;
-//     const [rows] = await List.update({
-//       title, status
-//     }, {
-//       where: {
-//         id, userId: req.user.id
-//       }
-//     });
-//     if (rows) return res.status(400).json({ message: 'fail to update list' });
-//     res.status(200).json({
-//       message: 'success update list'
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+exports.editFlight = async (req, res, next) => {
+  try {
+    // const { id } = req.params;
+    // const { title, status } = req.body;
+    const {
+      id,
+      destination,
+      returnDate,
+      arrivalDate,
+      departureDate } = req.body;
+
+    const rows = await Flight.update({
+      destination,
+      returnDate,
+      arrivalDate,
+      departureDate
+    }, {
+      where: {
+        id: id
+      }
+    });
+    if (!rows) return res.status(400).json({ message: 'fail to update list' });
+    res.status(200).json();
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.delFlight = async (req, res, next) => {
   try {
--- routes/scheduleFlightRoute.js
+++ routes/scheduleFlightRoute.js
@@ -7,6 +7,7 @@ const scheduleFlightController = require('../controller/scheduleFlightController
 router.get('/', scheduleFlightController.getAllFlight);
 router.get('/:flightId', scheduleFlightController.getFlightById);
 router.post('/', scheduleFlightController.createFlight);
+router.put('/:flightId', scheduleFlightController.editFlight);
 router.delete('/:flightId', scheduleFlightController.delFlight);
 
 
